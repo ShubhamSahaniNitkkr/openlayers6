4faf91087de624ad4d2f699a64c93bfb065c4aa8
ShubhamSahaniNitkkr <shubhamsahaninitkkr@gmail.com>
2022-02-13 12:07:11 +0530

    -multilayers

--- main.js
+++ main.js
@@ -1,78 +1,127 @@
 window.onload = init;
 
-function init() {
-  const fullScreenControl = new ol.control.FullScreen();
-  const mousePositionControl = new ol.control.MousePosition();
-  const overViewMapControl = new ol.control.OverviewMap({
-    collapsed: false,
-    layers: [
-      new ol.layer.Tile({
-        source: new ol.source.OSM(),
-      }),
-    ],
-  });
-  const scaleLineControl = new ol.control.ScaleLine();
-  const zoomSliderControl = new ol.control.ZoomSlider();
-  const zoomToExtendControl = new ol.control.ZoomToExtent();
+const attributionControl = new ol.control.Attribution({
+  collapsible: true,
+});
 
+function init() {
   const map = new ol.Map({
     view: new ol.View({
       center: [04721242, 3041234],
       zoom: 2,
-      //   maxZoom: 4,
-      //   minZoom: 2,
-      //   rotation: 0.45,
+      //   to limit the rea
+      //   extent: [
+      //     7565041.697447874,
+      //     811584.4625886064,
+      //     10792166.027911734,
+      //     4361421.226098852,
+      //   ],
     }),
     layers: [
       new ol.layer.Tile({
         source: new ol.source.OSM(),
+        zIndex: 1,
+        visible: false,
+        // exten:[LX,BY,RX,TY],
+        extent: [
+          7565041.697447874,
+          811584.4625886064,
+          10792166.027911734,
+          4361421.226098852,
+        ],
+        opacity: 0.5,
       }),
     ],
     target: "js-map",
-    keyboardEventTarget: document,
     controls: ol.control
-      .defaults()
-      .extend([
-        fullScreenControl,
-        mousePositionControl,
-        overViewMapControl,
-        scaleLineControl,
-        zoomSliderControl,
-        zoomToExtendControl,
-      ]),
+      .defaults({ attribution: false })
+      .extend([attributionControl]),
   });
 
-  //   overlay
-  const popupContainerElement = document.getElementById("popup-coordinates");
-  const popup = new ol.Overlay({
-    element: popupContainerElement,
-    positioning: "bottom",
+  // Layer group
+
+  const layerGroup = new ol.layer.Group({
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM({
+          url: "https://{a-c}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
+          zIndex: 0,
+          visible: false,
+          extent: [
+            7565041.697447874,
+            811584.4625886064,
+            10792166.027911734,
+            4361421.226098852,
+          ],
+          opacity: 0.5,
+        }),
+      }),
+      new ol.layer.Tile({
+        source: new ol.source.BingMaps({
+          key:
+            "Asd2p_SFbrqsiVxWAwjgGGOjR7uG-g5tof3pizdgmr7ckpCDKFlpcLq8z44LmQrk",
+          imagerySet: "AerialWithLabels", //Road, Canvas, CanvasDark, OrdanceSurvey
+        }),
+        visible: false,
+      }),
+    ],
   });
-  map.addOverlay(popup);
-  map.on("click", (e) => {
-    const clickedCoordinates = e.coordinate;
-    popup.setPosition(undefined);
-    popup.setPosition(clickedCoordinates);
-    popupContainerElement.innerHTML = clickedCoordinates;
+  map.addLayer(layerGroup);
+
+  //   cartoDB base Map
+  const cartoDBBaseLayer = new ol.layer.Tile({
+    source: new ol.source.XYZ({
+      url:
+        "http://{1-4}.basemaps.cartocdn.com/rastertiles/dark_all/{z}/{x}/{y}.png",
+    }),
+    visible: false,
+  });
+  map.addLayer(cartoDBBaseLayer);
+
+  //   TileDebug
+  const tileDebuglayer = new ol.layer.Tile({
+    source: new ol.source.TileDebug(),
+    visible: false,
+  });
+  map.addLayer(tileDebuglayer);
+
+  //   stemen layer
+  const stamenBaseLayer = new ol.layer.Tile({
+    source: new ol.source.XYZ({
+      url: "http://tile.stamen.com/terrain/{z}/{x}/{y}.jpg",
+    }),
+    visible: false,
+  });
+  map.addLayer(stamenBaseLayer);
+
+  //   argis layer
+  const tileArgisLayer = new ol.layer.Tile({
+    source: new ol.source.TileArcGISRest({
+      url:
+        "http://sampleserver1.arcgisonline.com/ArcGIS/rest/services/Demographics/ESRI_Population_World/MapServer",
+    }),
+    visible: false,
+  });
+  map.addLayer(tileArgisLayer);
+
+  //   novaa layer
+  const noaaWMSLayer = new ol.layer.Tile({
+    source: new ol.source.TileWMS({
+      url:
+        "https://nowcoast.noaa.gov/arcgis/services/nowcoast/forecast_meteoceanhydro_sfc_ndfd_dailymaxairtemp_offsets/MapServer/WMSServer?",
+      params: {
+        LAYERS: 5,
+        FORMAT: "image/png",
+        TRANSPARENT: true,
+      },
+      //   attributions: '<a href="https://nowcoast.noaa.gov">@NOVA</a>',
+    }),
+    visible: true,
   });
 
-  //   dragrotate interaction
-  const dragRotateInteraction = new ol.interaction.DragRotate({
-    condition: ol.events.condition.altKeyOnly,
-  });
-  map.addInteraction(dragRotateInteraction);
-
-  //freehand drawing on map
-  const drawInteraction = new ol.interaction.Draw({
-    type: "Polygon",
-    freehand: true,
-  });
-  //   disabling draw
-  //   map.addInteraction(drawInteraction);
-
-  drawInteraction.on("drawend", (e) => {
-    let parser = new ol.format.GeoJSON();
-    let drawnFeatures = parser.writeFeatures([e.feature]);
-    console.log(drawnFeatures);
-  });
+  noaaWMSLayer
+    .getSource()
+    .setAttributions('<a href="https://nowcoast.noaa.gov">@Shubham Sunny<a/>');
+  noaaWMSLayer.set("maxZoom", 5);
+  map.addLayer(noaaWMSLayer);
 }
